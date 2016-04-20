7a9af0590724edb9fd0d9b635aa2fbdef41e75fa
cquxutao <xutao1992319@hotmail.com>
2016-04-20 22:25:19 +0800

    xt,201604202244,first commit,UITableViewController basic usage practice.

--- TableViewDemo1/ViewController.m
+++ TableViewDemo1/ViewController.m
@@ -10,13 +10,41 @@
 
 @interface ViewController ()
 
+@property (nonatomic,strong) NSArray *arrayData;
+
 @end
 
 @implementation ViewController
+@synthesize arrayData;
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    arrayData=[NSArray arrayWithObjects:@"1",@"2",@"3",nil];
+    
+}
+#pragma mark - 表格1
+-(NSInteger) numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+    
+}
+-(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return arrayData.count;
+}
+#pragma mark - 表格2
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath
+{
+    static NSString *indentifier=@"cell";
+    UITableViewCell *cell=[tableView dequeueReusableCellWithIdentifier:indentifier];
+    if(!cell)
+    {
+        cell=[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:indentifier];
+    }
+    cell.textLabel.text=[arrayData objectAtIndex:indexPath.row];
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning {
